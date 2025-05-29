4aef3105c6b751e72ab184a0c662e5b2f14d8fc7
parthjpatel99 <parth8199@gmail.com>
2025-05-28 20:20:19 -0700

    Move files to utils for consistency
    Signed-off-by: parthjpatel99 <parth8199@gmail.com>

--- embabel-common-util/src/main/kotlin/com/embabel/common/util/StringTrimmingUtils.kt
+++ embabel-common-util/src/main/kotlin/com/embabel/common/util/StringTrimmingUtils.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.embabel.common.core.util
+package com.embabel.common.util
 
 const val DEFAULT_ELLIPSIS: String = "..."
 
