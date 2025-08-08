f6d57c54f4354e8b4a693fac6b43b0231cffcaad
Rod Johnson <johnsonroda@gmail.com>
2025-08-08 14:25:27 +1000

    Pull OptionsConverter into its own file

--- embabel-common-ai/src/main/kotlin/com/embabel/common/ai/model/Llm.kt
+++ embabel-common-ai/src/main/kotlin/com/embabel/common/ai/model/Llm.kt
@@ -20,32 +20,7 @@ import com.embabel.common.ai.prompt.PromptContributor
 import com.embabel.common.ai.prompt.PromptContributorConsumer
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import org.springframework.ai.chat.model.ChatModel
-import org.springframework.ai.chat.prompt.ChatOptions
-import org.springframework.ai.model.tool.ToolCallingChatOptions
 import java.time.LocalDate
-
-/**
- * Convert our LLM options to Spring AI ChatOptions
- */
-fun interface OptionsConverter<O : ChatOptions> {
-    fun convertOptions(options: LlmOptions): O
-}
-
-/**
- * Do not use in production code, this is just a lowest common denominator
- * and example
- */
-object DefaultOptionsConverter : OptionsConverter<ChatOptions> {
-    override fun convertOptions(options: LlmOptions): ChatOptions =
-        ToolCallingChatOptions.builder()
-            .temperature(options.temperature)
-            .topP(options.topP)
-            .maxTokens(options.maxTokens)
-            .presencePenalty(options.presencePenalty)
-            .frequencyPenalty(options.frequencyPenalty)
-            .topP(options.topP)
-            .build()
-}
 
 /**
  * Wraps a Spring AI ChatModel exposing an LLM.
